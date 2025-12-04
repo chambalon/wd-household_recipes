289a965f836cab2752d9c3d45be7c282e016d6a9
chambalon <nehachambalonk@gmail.com>
2025-12-04 22:30:38 +0800

    c9

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleSidebar(){
+  const sidebar = document.getElementById('sideBar');
+  sidebar.classList.toggle('visible');
+}
+
+
+/*
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+
+
+
+function showSidebar(){
+  const sidebarLinks = document.querySelectorAll('sidebarLink');
+  sidebarLinks.forEach(link=> {
+    link.style.display = 'block';
+  });
+}
+
+function hideSidebar(){
+  const sidebarLinks = document.querySelectorAll('sidebarLink');
+  sidebarLinks.forEach(link=> {
+    link.style.display = 'none';
+  });
+}
+*/
