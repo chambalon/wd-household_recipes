ea101891b3742706ebe4bb2b59c6edbe6f6da76c
chambalon <nehachambalonk@gmail.com>
2025-12-04 23:05:13 +0800

    c10

--- script.js
+++ script.js
@@ -3,26 +3,3 @@ function toggleSidebar(){
   sidebar.classList.toggle('visible');
 }
 
-
-/*
-function hideSidebar(){
-  const sidebar = document.querySelector('.sidebar');
-  sidebar.style.display = 'none';
-}
-
-
-
-function showSidebar(){
-  const sidebarLinks = document.querySelectorAll('sidebarLink');
-  sidebarLinks.forEach(link=> {
-    link.style.display = 'block';
-  });
-}
-
-function hideSidebar(){
-  const sidebarLinks = document.querySelectorAll('sidebarLink');
-  sidebarLinks.forEach(link=> {
-    link.style.display = 'none';
-  });
-}
-*/
