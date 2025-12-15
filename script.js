7511c29c4bfc0e3df62b97af4499dc4305522dd9
chambalon <nehachambalonk@gmail.com>
2025-12-15 11:07:26 +0800

    c11

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-fetch('navbar.html')
-.then(response => response.text())
-.then(data => {document.getElementById('navbar-container').innerHTML = data});
-
 
 function toggleSidebar(){
   const sidebar = document.getElementById('sideBar');
@@ -10,12 +6,3 @@ function toggleSidebar(){
 
 
 
-document.body.addEventListener('click', function(event) {
-  // Check if the clicked element (or any of its parents) has the 'menubutton' class
-  if (event.target.closest('.menubutton')) {
-      toggleSidebar();
-  }
-  if (event.target.closest('#closebutton')) {
-    toggleSidebar();
-}
-});
